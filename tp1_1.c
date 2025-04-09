0ce5639098bf53df9fab96e2dd4aa848ca7d16da
Rosario Molina <rosariomolina6463@gmail.com>
2025-04-09 01:24:59 -0300

    Punto 3 sobre punteros

--- tp1_1.c
+++ tp1_1.c
@@ -3,7 +3,25 @@
 int main()
 {
 
-    printf("Hola Mundo");
+    printf("Hola Mundo\n");
     
+    /*Declare un puntero en el procedimiento principal que apunte a una
+    variable de algún tipo y muestre por pantalla lo siguiente:
+    1) El contenido del puntero
+    2) La dirección de memoria almacenada por el puntero.
+    3) la dirección de memoria de la variable.
+    4) la dirección de memoria del puntero.
+    5) el tamaño de memoria utilizado por esa variable usando la
+    función sizeof() */ 
+
+    int numero = 10;
+    int *puntero = &numero;
+
+    printf("Contenido del puntero: %d\n", *puntero);
+    printf("Direccion almacenada por el puntero: %p\n", puntero);
+    printf("Direccion de memoria de la variable: %p\n", &numero);
+    printf("Direccion de memoria del puntero: %p\n", &puntero);
+    printf("Tamanio en memoria de la variable: %lu bytes\n", sizeof(numero));
+
     return 0;
 }
